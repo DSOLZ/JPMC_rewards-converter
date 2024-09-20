1aec1108619b7fe23e7422e6efebb0d8121898ae
David Sola-Adebiyi <144735635+DSOLZ@users.noreply.github.com>
2024-09-19 22:55:00 -0500

    Update RewardValue.java

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -2,11 +2,12 @@ public class RewardValue {
    
     private double cashValue;
     private double milesValue;
-
+    public static final double MILES_TO_CASH_CONVERSION_RATE = 0.0035;
+   
 
     public RewardValue(double cashValue) {
         this.cashValue = cashValue;
-        this.milesValue = cashValue * 0.0035;
+        this.milesValue = cashValue * MILES_TO_CASH_CONVERSION_RATE;
     }
 
     public double getCashValue() {
